ecd3e67b74224077a3de932970a0b812f5257efa
y.okamoto <okamoto.yuuki.1113+github@gmail.com>
2022-02-19 19:44:08 +0900

    Added AWS VPC CLI shellscript

---
+++ 00_vpc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# https://docs.aws.amazon.com/ja_jp/vpc/latest/userguide/vpc-subnets-commands-example.html
+
+
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/create-vpc.html
+aws --profile icey ec2 create-vpc --cidr-block 10.0.0.0/16 \
+                                  --instance-tenancy default \
+                                  --query Vpc.VpcId --output json \
+                                  --tag-specifications ResourceType=vpc,Tags='[{Key=Name,Value="okamoto test vpc"},{Key=Owner,Value="okamoto"}]'
+
+# 対象のVPCが作成されていることの確認
+aws --profile icey ec2 describe-vpcs
