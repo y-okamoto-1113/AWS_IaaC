78ada42232086a2a2b0bfe8647954679f5a23cd5
y.okamoto <okamoto.yuuki.1113+github@gmail.com>
2022-02-20 22:37:47 +0900

    Added the AWS CLI code about attaching IGW to VPC.

--- 20_internet_gateway.sh
+++ 20_internet_gateway.sh
@@ -2,3 +2,8 @@
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/create-internet-gateway.html
 
 aws --profile icey ec2 create-internet-gateway --tag-specifications ResourceType=internet-gateway,Tags='[{Key=Name,Value=okamoto-test-igw}]'
+
+# VPCにIGWをアタッチ
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/attach-internet-gateway.html
+aws --profile icey ec2 attach-internet-gateway  --internet-gateway-id igw-038e36ecb255bdbd7 \
+                                                --vpc-id vpc-0df9b38cad3adb436
